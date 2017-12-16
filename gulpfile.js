eb74abfa5331432a97f9443cd81fbbb93a4cf971
Pedro Nakashima <nakashimapedro@gmail.com>
2017-12-16 20:23:34 -0300

    Seção: 19; Aula: 55 (Support for SVG Icons in Legacy Browsers (Part 1))

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,6 @@ require('./gulp/tasks/styles.js');
 require('./gulp/tasks/watch.js');
 require('./gulp/tasks/sprites.js');
 require('./gulp/tasks/scripts.js');
+require('./gulp/tasks/modernizr.js');
 
 
